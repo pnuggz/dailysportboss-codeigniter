53d13d1ad585615c075e3f28eb6eba5a061ffa11
zerofighter <aditdwiputranto@gmail.com>
2016-10-25 01:56:35 +0700

    validation signup birthday fix

--- application/libraries/MY_Form_validation.php
+++ application/libraries/MY_Form_validation.php
@@ -1,9 +1,23 @@
 <?php
     if (!defined('BASEPATH')) exit('No direct script access allowed');
     class MY_Form_validation extends CI_Form_validation {
-        
+        function __construct($config = array())
+        {
+          parent::__construct($config);
+        }
+
+        function error_array()
+        {
+          if (count($this->_error_array) === 0)
+            return FALSE;
+          else
+            return $this->_error_array;
+        }
+
         function run($module = '', $group = '') {
             (is_object($module)) AND $this->CI = &$module;
             return parent::run($group);
         }
+
+        
     }
--- application/modules/signup/controllers/signup.php
+++ application/modules/signup/controllers/signup.php
@@ -18,17 +18,22 @@ class Signup extends MX_Controller {
 
            $this->form_validation->set_rules('first_name', 'First Name', 'trim|required|max_length[30]|xss_clean');
            $this->form_validation->set_rules('last_name', 'Last Name', 'trim|required|max_length[30]|xss_clean');
-           $this->form_validation->set_rules('email', 'Email', 'trim|required|max_length[50]|xss_clean|valid_email|is_unique[users.email]');
-           $this->form_validation->set_rules('username', 'Username', 'trim|required|max_length[30]|xss_clean|is_unique[users.username]');
+           $this->form_validation->set_rules('email', 'Email', 'trim|required|max_length[50]|xss_clean|valid_email');
+           $this->form_validation->set_rules('username', 'Username', 'trim|required|max_length[30]|xss_clean');
            $this->form_validation->set_rules('password', 'Password', 'required|max_length[30]|xss_clean');
            $this->form_validation->set_rules('password2', 'Confirm Password', 'required|max_length[30]|xss_clean|matches[password]');
            $this->form_validation->set_rules('address', 'Address', 'trim|required|max_length[512]|xss_clean');
            $this->form_validation->set_rules('mobilephone', 'Mobile Phone', 'numeric|trim|required|max_length[512]|xss_clean');
-           $this->form_validation->set_rules('birthday', 'Birthday', 'trim|required|regex_match[/^(19|20)\d\d[- /.](0[1-9]|1[012])[- /.](0[1-9]|[12][0-9]|3[01])$/]');
+           $this->form_validation->set_rules('birthday', 'Birthday', 'trim|required');
+           if($this->checkDateFormat($this->input->post('birthday')) == false){
+             return $this->output->set_output(json_encode(array('error'=>array('birthday'=>"Invalid format birthday date"))), 200);
+           }
+
            if($this->form_validation->run() == FALSE)
            {
-             print_r(validation_errors());exit;
-               $this->output->set_output(json_encode(), 200);
+
+              $data = array('error'=>$this->form_validation->error_array());
+               $this->output->set_output(json_encode($data), 200);
 
            } else {
              $this->load->model('mdl_users');
@@ -38,13 +43,14 @@ class Signup extends MX_Controller {
 
              if($cekusername > 0)
              {
-               $this->output->set_output(json_encode("Username Already Exists"), 200);
+               $this->output->set_output(json_encode(array('error'=>array('message'=>"Username Already Exists"))), 200);
              }else if($cekemail > 0)
              {
-               $this->output->set_output(json_encode("Email Already Exists"), 200);
+               $this->output->set_output(json_encode(array('error'=>array('message'=>"Email Already Exists"))), 200);
              }else{
                $enc_password = md5($this->input->post('password'));
-
+               $sub = $this->input->post('subscribe');
+               if(!$sub)$sub=0;
                $data = array('first_name'      =>       $this->input->post('first_name'),
                              'last_name'    =>      $this->input->post('last_name'),
                              'email'    =>      $this->input->post('email'),
@@ -53,14 +59,25 @@ class Signup extends MX_Controller {
                              'address'    =>      $this->input->post('address'),
                              'phonenumber'     =>      $this->input->post('mobilephone'),
                              'birthday'     =>      date('Y-m-d',strtotime($this->input->post('birthday'))),
-                             'subscribe'     =>      $this->input->post('subscribe')
+                             'subscribe'     => $sub
                             );
 
                $this->_insert($data);
-               $this->output->set_output(json_encode("1"), 200);
+               $this->output->set_output(json_encode(array('success'=>array('message'=>"Registration Success"))), 200);
              }
            }
        }
+
+       function checkDateFormat($date) {
+          if (preg_match('/^\d{2}\-\d{2}\-\d{4}$/', $date)) {
+              if(checkdate(substr($date, 3, 2), substr($date, 0, 2), substr($date, 6, 4)))
+                  return true;
+              else
+                  return false;
+          } else {
+              return false;
+          }
+      }
 
        function _insert($data) {
            $this->load->model('mdl_users');
